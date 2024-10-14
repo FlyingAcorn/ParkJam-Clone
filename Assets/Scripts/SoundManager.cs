53c2ffe7eb61065798f29526a8075d037f5fd9a5
Onur Rasna <154007465+FlyingAcorn@users.noreply.github.com>
2024-10-14 20:36:08 +0300

    Made small adjustments to soundmanager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SoundManager : Singleton<SoundManager>
 {
     public AudioSource audioSource;
+    private AudioClip _sfx; 
     [SerializeField] private AudioClip[] audioClips;
     //TODO: Honking when collision,some music,victory sound also add confetti,car crash sound for obstacles and hitting cars
     //TODO: also for touching buttons
@@ -10,8 +11,10 @@ public class SoundManager : Singleton<SoundManager>
 
     public void PlaySfx(int clip)
     {
-        var sfx = audioClips[clip];
-        audioSource.PlayOneShot(sfx);
+        if (audioSource.isPlaying && audioClips[clip] == _sfx) return;
+        _sfx = audioClips[clip];
+        audioSource.PlayOneShot(_sfx);
+        Debug.Log("playing sound");
 
     }
     
